349c6340be01e45ef2f0012102980dc2e4743c7e
Shelby Doolittle <shelby@shelbyd.com>
2022-03-06 21:52:54 -0500

    Switch to Ace3

--- main.lua
+++ main.lua
@@ -1,9 +1,20 @@
-local EventsTriggeringRecalc = {
-    "PLAYER_ENTERING_WORLD",
-    "PLAYER_REGEN_DISABLED", -- Enter combat
-    "PLAYER_REGEN_ENABLED", -- Leave combat
-    "GROUP_ROSTER_UPDATE",
-};
+SummonStone = LibStub("AceAddon-3.0"):NewAddon("SummonStone", "AceConsole-3.0")
+local AceGUI = LibStub("AceGUI-3.0")
+
+function SummonStone:OnInitialize()
+end
+
+local ShouldRender = true
+
+SummonStone:RegisterChatCommand("ss", "OnSlashCommand")
+function SummonStone:OnSlashCommand(input)
+    ShouldRender = true
+    Render()
+end
+
+local EventsTriggeringRecalc = {"PLAYER_ENTERING_WORLD", "PLAYER_REGEN_DISABLED", -- Enter combat
+"PLAYER_REGEN_ENABLED", -- Leave combat
+"GROUP_ROSTER_UPDATE"};
 
 local EventFrame = CreateFrame("Frame", "SummonStone_EventFrame")
 
@@ -12,67 +23,62 @@ for _index, event in pairs(EventsTriggeringRecalc) do
 end
 
 EventFrame:SetScript("OnEvent", function(self, event, ...)
-    print("Got event", event)
-
-    Luact.Render(Main, UIParent)
+    Render()
 end)
 
-function Main()
+local CurrentSummonFrame = nil
+
+function Render()
+    if CurrentSummonFrame ~= nil then
+        -- Need to nil out CurrentSummonFrame so we can detect if the
+        -- close button was clicked below
+        local frame = CurrentSummonFrame
+        CurrentSummonFrame = nil
+        AceGUI:Release(frame)
+    end
+
+    if not ShouldRender then
+        return
+    end
     if UnitAffectingCombat("player") then
-        return nil
+        return
     end
 
-    local needingSummon = RaidersNeedingSummon()
-    print('needingSummon', needingSummon)
-    if table.getn(needingSummon) == 0 then
-        return nil
+    local needSummon = RaidersNeedingSummon()
+    if table.getn(needSummon) == 0 then
+        return
     end
 
-    return MainFrame({
-        title = "SummonStone"
-    }, {ToSummonNames({ raiderIndices = needingSummon })})
-end
-
-function MainFrame(props, children)
-    return Luact.Frame({
-        id = "SummonStone_MainFrame",
-        inherit = "BasicFrameTemplateWithInset",
-        size = {300, 300},
-        clearPoints = false,
-        point = "CENTER",
-        resizable = true,
-        movable = true,
-        MinResize = {100, 100},
-        MaxResize = {500, 500}
-    }, {Luact.Button({
-        id = "SummonStone_Resize",
-        point = "BOTTOMRIGHT",
-        size = {16, 16},
-
-        textures = {
-            normal = "Interface\\ChatFrame\\UI-ChatIM-SizeGrabber-Down",
-            highlight = "Interface\\ChatFrame\\UI-ChatIM-SizeGrabber-Highlight",
-            pushed = "Interface\\ChatFrame\\UI-ChatIM-SizeGrabber-Up"
-        },
-        events = {
-            OnMouseDown = function(self)
-                self:GetParent():StartSizing("BOTTOMRIGHT")
-            end,
-            OnMouseUp = function(self)
-                self:GetParent():StopMovingOrSizing()
-            end
-        }
-    }), Luact.Text(props.title, {
-        fontObject = "GameFontHighlight",
-        point = function(parent)
-            return "LEFT", parent.TitleBg, "LEFT", 5, 0
+    CurrentSummonFrame = AceGUI:Create("Frame")
+    CurrentSummonFrame:SetTitle("SummonStone")
+    CurrentSummonFrame:SetStatusText("Get your team summoned!")
+    CurrentSummonFrame:SetCallback("OnClose", function(widget, ...)
+        local closeButtonClicked = CurrentSummonFrame ~= nil
+        if closeButtonClicked then
+            ShouldRender = false
         end
-    })})
+        CurrentSummonFrame = nil
+        AceGUI:Release(widget)
+    end)
+    CurrentSummonFrame:SetAutoAdjustHeight(true)
+    CurrentSummonFrame:SetLayout("Flow")
+
+    local heading = AceGUI:Create("Heading")
+    heading:SetText("Needing Summon")
+    heading:SetRelativeWidth(1)
+    CurrentSummonFrame:AddChild(heading)
+
+    for i, raiderIndex in ipairs(needSummon) do
+        local name = select(1, GetRaidRosterInfo(raiderIndex))
+        local label = AceGUI:Create("Label")
+        label:SetText(name)
+        CurrentSummonFrame:AddChild(label)
+    end
 end
 
 function RaidersNeedingSummon()
     local needsSummoned = {}
-    for i=1,GetNumGroupMembers() do
+    for i = 1, GetNumGroupMembers() do
         if DoesRaiderNNeedSummon(i) then
             table.insert(needsSummoned, i)
         end
@@ -83,22 +89,10 @@ end
 function DoesRaiderNNeedSummon(n)
     local playerZone = GetZoneText()
     local raiderZone = select(7, GetRaidRosterInfo(n))
+    local name = select(1, GetRaidRosterInfo(n))
     if playerZone == raiderZone then
         return false
     else
         return true
     end
 end
-
-function ToSummonNames(props)
-    local children = {}
-    for i, raiderIndex in ipairs(props.raiderIndices) do
-        local name = select(1, GetRaidRosterInfo(raiderIndex))
-        table.insert(children, Luact.Text(name))
-    end
-    return Luact.Frame({
-        size = {140, 32},
-    }, children)
-end
-
-Luact.Render(Main, UIParent)
