02389e28a63a4f5adc692c719a2a341b2edafe44
Shelby Doolittle <shelby@shelbyd.com>
2022-03-07 00:57:45 -0500

    Clean up unused rendering code

--- main.lua
+++ main.lua
@@ -1,105 +1,6 @@
 SummonStone = LibStub("AceAddon-3.0"):NewAddon("SummonStone", "AceConsole-3.0")
 local AceGUI = LibStub("AceGUI-3.0")
 
-function SummonStone:OnInitialize()
-end
-
-local ShouldRender = true
-
-SummonStone:RegisterChatCommand("ss", "OnSlashCommand")
-function SummonStone:OnSlashCommand(input)
-    ShouldRender = true
-    Render()
-end
-
-local EventsTriggeringRecalc = {
-    "PLAYER_ENTERING_WORLD",
-    "PLAYER_REGEN_DISABLED", -- Enter combat
-    "PLAYER_REGEN_ENABLED", -- Leave combat
-    "GROUP_ROSTER_UPDATE",
-};
-
-local EventFrame = CreateFrame("Frame", "SummonStone_EventFrame")
-
-for _index, event in pairs(EventsTriggeringRecalc) do
-    EventFrame:RegisterEvent(event)
-end
-
-EventFrame:SetScript("OnEvent", function(self, event, ...)
-    -- Render()
-end)
-
-local CurrentSummonFrame = nil
-
-function Render()
-    if CurrentSummonFrame ~= nil then
-        -- Need to nil out CurrentSummonFrame so we can detect if the
-        -- close button was clicked below
-        local frame = CurrentSummonFrame
-        CurrentSummonFrame = nil
-        AceGUI:Release(frame)
-    end
-
-    if not ShouldRender then
-        return
-    end
-    if IsInCombat() then
-        return
-    end
-
-    local needSummon = RaidersNeedingSummon()
-    if table.getn(needSummon) == 0 then
-        return
-    end
-
-    CurrentSummonFrame = AceGUI:Create("Frame")
-    CurrentSummonFrame:SetTitle("SummonStone")
-    CurrentSummonFrame:SetStatusText("Get your team summoned!")
-    CurrentSummonFrame:SetCallback("OnClose", function(widget, ...)
-        local closeButtonClicked = CurrentSummonFrame ~= nil
-        if closeButtonClicked then
-            ShouldRender = false
-        end
-        CurrentSummonFrame = nil
-        AceGUI:Release(widget)
-    end)
-    CurrentSummonFrame:SetAutoAdjustHeight(true)
-    CurrentSummonFrame:SetLayout("Flow")
-
-    local heading = AceGUI:Create("Heading")
-    heading:SetText("Needing Summon")
-    heading:SetRelativeWidth(1)
-    CurrentSummonFrame:AddChild(heading)
-
-    for i, raiderIndex in ipairs(needSummon) do
-        RenderSummonNeeded(raiderIndex, CurrentSummonFrame)
-    end
-end
-
-function IsInCombat()
-    return UnitAffectingCombat("player")
-end
-
-function RaidersNeedingSummon()
-    local needsSummoned = {}
-    for i = 1, GetNumGroupMembers() do
-        if DoesRaiderNNeedSummon(i) then
-            table.insert(needsSummoned, i)
-        end
-    end
-    return needsSummoned
-end
-
-function RenderSummonNeeded(raiderIndex, parent)
-    local name = select(1, GetRaidRosterInfo(raiderIndex))
-    SummonStone:Print(GetRaidRosterInfo(raiderIndex))
-
-    local label = AceGUI:Create("Label")
-    label:SetText(name)
-    label:SetRelativeWidth(1)
-    parent:AddChild(label)
-end
-
 local TargetButtonName = "SummonStone_TargetButton";
 
 local InTargetButtonHideDelay = false
@@ -127,6 +28,10 @@ function MaybeShowTargetButton()
     end
 
     return HideTargetButton()
+end
+
+function IsInCombat()
+    return UnitAffectingCombat("player")
 end
 
 local TargetButton = CreateFrame("Button", TargetButtonName, UIParent, "GameMenuButtonTemplate,SecureActionButtonTemplate")
